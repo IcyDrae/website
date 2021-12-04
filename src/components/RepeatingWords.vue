6214c55ef6e656b9ef0ee2a7ec73b72224b1b71c
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-12-04 18:57:17 +0100

    refactor: cleaned up vuex global store

--- src/components/RepeatingWords.vue
+++ src/components/RepeatingWords.vue
@@ -1,28 +1,26 @@
 <template>
   <div class="repeating-words-container">
-      <p v-for="word in endlessWords" v-bind:key="word.id"> {{ word.content }} </p>
-      <p v-for="word in endlessWords" v-bind:key="word.id"> {{ word.content }} </p>
+      <p v-for="word in words" v-bind:key="word.id"> {{ word.content }} </p>
+      <p v-for="word in words" v-bind:key="word.id"> {{ word.content }} </p>
   </div>
 </template>
 
 <script>
 
-import { mapGetters } from 'vuex';
-
 export default {
-    name: "RepeatingWords",
-    data() {
-        return {
-        }
-    },
-    computed: {
-        ...mapGetters(['endlessWords'])
-    },
-    methods: {
-    },
+  name: "RepeatingWords",
+  data() {
+    return {
+      words: [
+        { content: 'Trust' },
+        { content: 'Gratitude' },
+        { content: 'Mercy' },
+        { content: 'Authority' },
+        { content: 'All-Encompassing' },
+        { content: 'Beauty' },
+        { content: 'Return' },
+      ],
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,40 +2,12 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    isActive: false,
-    logoText: 'nur fide',
-    words: [
-      { content: 'Trust' },
-      { content: 'Gratitude' },
-      { content: 'Mercy' },
-      { content: 'Authority' },
-      { content: 'All-Encompassing' },
-      { content: 'Beauty' },
-      { content: 'Return' },
-    ],
   },
   getters: {
-    activeState: state => state.isActive,
-    logoState: state => state.logoText,
-    endlessWords: state => state.words,
   },
   mutations: {
-    setIsActive(state, boolean) {
-      state.isActive = boolean
-    },
-    setLogoText(state, text) {
-      if (this.state.isActive) {
-        state.logoText = text
-      } else if(!this.state.isActive || this.state.isActive == undefined) {
-        state.logoText = 'nur fide'
-      }
-    },
   },
   actions: {
-    showOverlay(context) {
-      context.commit('setIsActive', this.boolean = !this.boolean)
-      context.commit('setLogoText', 'authority')
-    },
   },
   modules: {}
 });
